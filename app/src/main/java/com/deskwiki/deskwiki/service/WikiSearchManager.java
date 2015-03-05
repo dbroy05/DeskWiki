8d1555e11d55372ebffa302ab45cadf6e60f57db
dbroy05 <dibyenduroy@Dibyendus-MacBook-Pro.local>
2015-03-05 13:38:27 -0500

    Added caching

--- app/src/main/java/com/deskwiki/deskwiki/service/WikiSearchManager.java
+++ app/src/main/java/com/deskwiki/deskwiki/service/WikiSearchManager.java
@@ -5,7 +5,9 @@ import com.deskwiki.deskwiki.model.WikiItem;
 import com.deskwiki.deskwiki.model.WikiResponse;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * WikiSearchManager handles all the query and delegates to Service Task to process, once retrieved,
@@ -15,14 +17,21 @@ import java.util.List;
  */
 public class WikiSearchManager {
     private static WikiResponse wikiResponse;
+    private static Map<String, WikiResponse> searchCache = new HashMap<>();
 
     /**
      * Returns the search result based on query and pageoffset. On success, it sets the result on listener.
+     * It maintains a cache based on query term for first page
      * @param query
      * @param pageOffset
      * @param listener
      */
-    public void getSearchResult(String query, int pageOffset, final WikiResponseListener listener){
+    public void getSearchResult(final String query, final int pageOffset, final WikiResponseListener listener){
+        WikiResponse cachedResult = searchCache.get(query);
+        //If its first page and cache hit, return it
+        if(pageOffset==0 && cachedResult != null){
+            listener.onSuccessResult(cachedResult);
+        }
 
         String[] queryParams = {query,String.valueOf(pageOffset)};
         //Fires off the main task for searching
@@ -31,6 +40,8 @@ public class WikiSearchManager {
             @Override
             public void onSuccessResult(WikiResponse resp) {
                 wikiResponse = resp;
+                if(pageOffset==0)
+                    searchCache.put(query,resp);
                 listener.onSuccessResult(resp);
             }
 
